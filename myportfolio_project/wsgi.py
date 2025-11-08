0d7c984294c14aca98d4d37fd07e2082dc2b3cff
Kegits <kelvingithaithi@gmail.com>
2025-11-08 12:38:01 +0300

    made some changes

--- myportfolio_project/wsgi.py
+++ myportfolio_project/wsgi.py
@@ -1,5 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportfolio_project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myportfolio_project.settings")
+
 application = get_wsgi_application()
+
+# 👇 Add this line for Vercel
+app = application
